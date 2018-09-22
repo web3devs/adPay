0757f377f6bfa01a78889bc6645a213658ba2c2b
Aaron Anderson <aaronlynanderson@gmail.com>
2018-09-22 15:03:14 -0400

    app is stable, but contract: results.web3.eth.contract(abi).at(address) returns undefined

--- controllers/rpc.controller.js
+++ controllers/rpc.controller.js
@@ -2,6 +2,7 @@ const chalk = require('chalk')
 const getWeb3 = require('../utils/getWeb3');
 let abi = require('../abis/abi.js');
 let address = require('../addresses/address');
+let contract;
 
 exports.get_web3 = async function () {
 getWeb3
@@ -12,5 +13,21 @@ getWeb3
   .catch(error => {
     console.log(chalk.red("*** Web3 not found ***"));
     return error;
+  })
+}
+
+exports.get_balance = async function (req, res) {
+  getWeb3
+  .then( results => {
+    // contract: results.web3.eth.contract(abi).at(address);
+    console.log(results);
+    res.status(results.status >= 100 && results.status < 600 ? err.code : 500)
+      .send("results");
+  })
+  .catch( error => {
+    console.log(chalk.white("oops in get_balance"));
+    console.log(error);
+    res.status(error.status >= 100 && error.status < 600 ? err.code : 500)
+      .send(JSON.stringify(error));
   })
 }
