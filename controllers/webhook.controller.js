0752974a3341dcaf7a4bc5d676324b8ed92f8057
SamSteele01 <ssteele017@gmail.com>
2018-09-23 11:39:41 -0400

    import rpc controller

--- controllers/webhook.controller.js
+++ controllers/webhook.controller.js
@@ -2,25 +2,28 @@
 
 const mongoose = require ( 'mongoose' );
 const AnswerLog = mongoose.model ( 'AnswerLog' );
-const webhook_controller = require('../controllers/webhook.controller');
+const rpc_controller = require('../controllers/rpc.controller');
 const chalk = require ( 'chalk' );
 
 exports.submitForm = function(req, res){
   console.log(req.body); // object
+
   // check that form is complete
   values = Object.values(req.body); // array
+
   if (!values.includes(null)) {
     // save form data to DB
     let answerData = new AnswerLog(req.body);
+
     answerData.save(function(err, data) {
       if (err) {
         res.send(err);
         console.log('error in saving answer data', err);
       } else {
-        // send response to typeform
+        // send response to form
         res.sendStatus(200);
         // fire contract function. Need: { receiverAddress }.
-
+        rpc_controller.pay(req.body.address)
         // Seperate endpoint or parse req.body?
 
       }
--- routes/webhook.route.js
+++ routes/webhook.route.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const wh_controller = require('../controllers/webhook.controller');
 // const mongo_controller = require('../controllers/mongo.controller');
 
-// webhook for formsUrls
+/* webhook for formsUrls */
 // (post) https://api.typeform.com/forms/{form_id}/webhooks/{tag}
 router.route('/forms/:formId/webhooks/:tag')
   .post(wh_Controller.submitForm)
