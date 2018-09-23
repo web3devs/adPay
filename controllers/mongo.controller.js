d3009a26c7d1c389fccd5b40e8993284596e35ce
insideoutzombie <david@web3devs.com>
2018-09-23 10:22:29 -0500

    add post & save to the db logic in mongo.controller.js

--- controllers/mongo.controller.js
+++ controllers/mongo.controller.js
@@ -2,22 +2,22 @@ const mongoose = require ( 'mongoose' );
 const Ad = mongoose.model ( 'Ad' );
 const chalk = require ( 'chalk' );
 
-// exports.find_by_id = async function (req, res) {
-//   let dbData;
-//   await Ad.findById( req.body._id, function (err,response){
-//       if (err) {
-//         console.log('TestThree3', err);
-//       } else {
-//         dbData = response;
-//         console.log('TestTwo2', dbData)
-//       }
-//     }
-//   );
-//   res.send(dbData);
-//   console.log('TestOne1', dbData);
-// }
+exports.find_by_id = async function (req, res) {
+  let dbData;
+  await Ad.findById( req.body._id, function (err,response){
+      if (err) {
+        console.log('TestThree3', err);
+      } else {
+        dbData = response;
+        console.log('TestTwo2', dbData)
+      }
+    }
+  );
+  res.send(dbData);
+  console.log('TestOne1', dbData);
+}
 
-// =========================================================================
+// ===========================================================================
 // THE code below is what posts to the DB!!!!! Huge break through for me!!!!
 // When I make a post call with postman url block/api
 // module.exports = {
@@ -29,20 +29,42 @@ const chalk = require ( 'chalk' );
 // },
 
 // THE code below is what posts to the DB!!!!!
-// When I make a post call with postman url 
-  exports.find_by_id = async function (req, res) {
-    let dbData;
-    db = dbData;
-    db.Ad.save(web3);
-    res.send(CircularJSON.stringify(web3));
-    request
-      .post('/ad/api')
-      .send({ text: 'mega Mcdonalds monster mayham' })
-    const blockProps = req.body;
+// When I make a post call with postman url
 
-    Block.create(blockProps)
-      .then(block => res.send(block));
+
+
+  exports.new_transaction = async function (req, res) {
+    let dbData;
+    let newAd = new Ad(
+      {
+      field1: '',
+      field2: '',
+      field3: '',
+      field4: '',
+      field5: '',
+      field6: '',
+      address1: '',
+      address2: ''
+    }
+  );
+  newAd.save ( function ( err, dbResponse ) {
+    if ( err ) {
+      res.send( err );
+    }
+    console.log ( "***" + chalk.white( dbResponse ) + "***" );
+    res.send ( dbResponse );
+  });
 }
+//
+//     res.send(CircularJSON.stringify());
+//     request
+//       .post('/ad/api')
+//       .send({ text: 'mega Mcdonalds monster mayham' })
+//     const blockProps = req.body;
+//
+//     Block.create(adProps)
+//       .then(ad => res.send(ad));
+// }
 
 // =============================================================================
 exports.find_all = async function (req, res) {
